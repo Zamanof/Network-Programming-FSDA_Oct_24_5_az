5f1532de3bf2b9049320cecbc65cbcd589be759c
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-10-09 18:10:24 +0400

    Mail

---
+++ NP 07. FTP/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+
+var ftpHostRoot = "ftp://eu-central-1.sftpcloud.io:21/";
+List<string> filenames = [
+    "abbas.txt",
+    "Murad.txt",
+    "istenilen ad.png",
+    "tuqay.txt"
+    ];
+//UploadFTP(ftpHostRoot);
+GetFTP(ftpHostRoot);
+
+//filenames.ForEach(f =>
+//{
+//    DownloadFTP(ftpHostRoot, f);
+//});
+//DownloadFTP(ftpHostRoot);
+void GetFTP(string url) {
+    var request = WebRequest.Create(url) as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+    request.Credentials =
+       new NetworkCredential("35fbb7c60e0e4c3fbb71a0c9797512d4", "fIBBMg9ko8Rf8Hmn9g883eTHU9cPH9iU");
+
+    var response = request.GetResponse() as FtpWebResponse;
+
+    var stream = response!.GetResponseStream();
+    var reader = new StreamReader(stream);
+    var data = reader.ReadToEnd();
+    Console.WriteLine(data);
+}
+void UploadFTP(string url) {
+    var request = WebRequest.Create($"{url}statusCode.jpg") as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.UploadFile;
+    request.Credentials =
+        new NetworkCredential("908a9b8e4a4544458f383296e4fd0f5f", "ULHvMfRS14dahTTmWB9Vv9G2VPYyxpvC");
+
+    var fileStream =
+        new FileStream(@"C:\Users\zamanov\Downloads\1619338681039.jpg", FileMode.Open);
+    var stream = request.GetRequestStream();
+    fileStream.CopyTo(stream);
+
+    stream.Close();
+    fileStream.Close();
+}
+void DownloadFTP(string url, string fileName) {
+    var request = WebRequest.Create($"{url}{fileName}") as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.DownloadFile;
+    request.Credentials =
+        new NetworkCredential("35fbb7c60e0e4c3fbb71a0c9797512d4", "fIBBMg9ko8Rf8Hmn9g883eTHU9cPH9iU");
+
+    var fileStream 
+        = new FileStream(@$"C:\Users\zamanov\Downloads\ftpDownload\{fileName}", FileMode.Create);
+
+    var response = request.GetResponse() as FtpWebResponse;
+    var stream = response.GetResponseStream();
+    stream.CopyTo(fileStream);
+    fileStream.Close();
+    stream.Close();
+}
---
+++ NP 07. Mail Protocols/Program.cs
@@ -0,0 +1,70 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+//using System.Net;
+//using System.Net.Mail;
+
+//SMTP();
+SMTPMailKit();
+
+//void SMTP() {
+//    var client = new SmtpClient("smtp.gmail.com", 587);
+//    client.EnableSsl = true;
+
+//    client.Credentials = new NetworkCredential("fbms.1223@gmail.com", "adppsroljzdrznri");
+
+//    var message = new MailMessage
+//    {
+//        Subject = "Test",
+//        Body = "WINTER IS COMING!!! (WINTER = EXAM)"
+//    };
+//    message.From = new MailAddress("fbms.1223@gmail.com", "Qurban Qurbanov");
+
+//    message.To.Add(new MailAddress("romandadasov13@gmail.com"));
+//    message.To.Add(new MailAddress("abbasesgerli343@gmail.com"));
+//    message.To.Add(new MailAddress("ayxanm803@gmail.com"));
+//    message.To.Add(new MailAddress("zamanov@itstep.org"));
+
+//    client.Send(message);
+
+//}
+
+
+void SMTPMailKit()
+{
+    var client = new SmtpClient();
+    client.Connect("smtp.gmail.com", 587);
+    client.Authenticate("fbms.1223@gmail.com", "adppsroljzdrznri");
+
+    var message = new MimeKit.MimeMessage();
+
+    message.From.Add(new MailboxAddress("Camel from Refrigirator", "fbms.1223@gmail.com"));
+
+    message.To.AddRange(new[]
+    {
+        MailboxAddress.Parse("romandadasov13@gmail.com"),
+        MailboxAddress.Parse("abbasesgerli343@gmail.com"),
+        MailboxAddress.Parse("ayxanm803@gmail.com"),
+        MailboxAddress.Parse("zamanov@itstep.org")
+    });
+    message.Subject = "Exam";
+    //message.Body = new TextPart("plain")
+    //{
+    //    Text = "WINTER IS COMING!!!(WINTER = EXAM)"
+    //};
+
+    message.Body = new TextPart("html")
+    {
+        Text = """
+        <h1 style='color:blue;'>WINTER IS COMING!!!(WINTER = EXAM)</h1>
+        <br/>
+        <img src='https://upload.wikimedia.org/wikipedia/en/1/18/Wana_Decrypt0r_screenshot.png'/>
+        <br/>
+        <a href="https://pranx.com/hacker/" style="display:inline-block;padding:10px 18px;background:#e53935;color:#fff;text-decoration:none;border-radius:6px;font-weight:600;border:1px solid rgba(0,0,0,0.15);box-shadow:0 2px 0 rgba(0,0,0,0.08);" role="button" aria-label="Dont Click">Dont Click</a>
+        """
+    };
+
+
+    client.Send(message);
+    client.Disconnect(true);
+}
+void IMAP() { }
